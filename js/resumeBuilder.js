ca7e5ee53aac337ce5ca62ab17cb69c8d64e07ed
Chandler9Wilson <chandler9wilson@gmail.com>
2016-03-22 15:39:08 -0500

    experement with gulp, added education object

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,29 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+    "name" : "Chandler Wilson",
+    "role" : "Student",
+    "contacts" : {
+        "mobile" : "512-585-8163",
+        "email" : "chandler9wilson@gmail.com",
+        "github" : "github.com/chandler9wilson",
+        "location" : "Austin"
+    },
+    "pic" : "images/chandler_pic.jpeg"
+}
+
+var education = {
+    "schools" : {
+        "name" : "HSANA",
+        "location" : "Pflugerville",
+        "dates" : "2013-2015",
+        "url" : "hsana.org"
+    },
+    "onlineCourses" : {
+        "title" : "Front-End Nanodegree",
+        "school" : "Udacity",
+        "date" : "2016",
+        "url" : "udacity.com"
+    }
+}
+
+
+
