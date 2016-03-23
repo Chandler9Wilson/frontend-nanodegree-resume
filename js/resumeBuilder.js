80bcf3c84e875f859d3d644369c7a9305496228f
Chandler9Wilson <chandler9wilson@gmail.com>
2016-03-23 13:40:04 -0500

    bio display working

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,8 +7,40 @@ var bio = {
         "github" : "github.com/chandler9wilson",
         "location" : "Austin"
     },
-    "pic" : "images/chandler_pic.jpeg"
+    "welcomeMessage" : "Hello",
+    "skills" : ["web-development", "gulp", "sass"],
+    "biopic" : "images/chandler_pic.jpeg",
+    display: function(){
+        var formattedName = HTMLheaderName.replace("%data%", bio.name);
+        var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+        var formattedBioPic = HTMLbioPic.replace("%data%", bio.biopic);
+        var formattedWelcomeMsg = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+
+        var formattedContactInfo = [];
+        formattedContactInfo.push(HTMLemail.replace("%data%", bio.contacts.email));
+        formattedContactInfo.push(HTMLgithub.replace("%data%", bio.contacts.github));
+        formattedContactInfo.push(HTMLlocation.replace("%data%", bio.contacts.location));
+
+        $("#header").prepend(formattedRole);
+        $("#header").prepend(formattedName);
+        $("#header").append(formattedBioPic);
+        $("#header").append(formattedWelcomeMsg);
+
+        for(i in formattedContactInfo) {
+            $("#topContacts").append(formattedContactInfo[i]);
+            $("#footerContacts").append(formattedContactInfo[i]);
+        }
+
+        if(bio.skills.length > 0) {
+            $("#header").append(HTMLskillsStart);
+            for(var i = 0; i < bio.skills.length; i++) {
+                $("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+            }
+        }
+    }
 }
+
+bio.display();
 
 var education = {
     "schools" : {
@@ -22,8 +54,35 @@ var education = {
         "school" : "Udacity",
         "date" : "2016",
         "url" : "udacity.com"
+    },
+    display: function(){
+
     }
 }
 
+var work = {
+    "jobs" : {
+        "employer" : "Harbor Freight Tools",
+        "title" : "sales associate",
+        "location" : "Austin",
+        "dates" : "2014-2015",
+        "description" : "Worked as a cashier and on the sales floor"
+    },
+    display: function(){
+
+    }
+}
+
+var projects = {
+    "projects" : {
+        "title" : "frogger-clone",
+        "dates" : "2016",
+        "description" : "A reskinned frogger clone created in javascript",
+        "images" : "https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Frogger_game_arcade.png/220px-Frogger_game_arcade.png"
+    },
+    display: function(){
+
+    }
+}
 
 
