95eff021aee5c949a8c2169845dc1a8e7e2f2196
Chandler <chandler9wilson@gmail.com>
2016-04-28 15:07:01 -0500

    Google Maps working

--- js/helper.js
+++ js/helper.js
@@ -236,12 +236,8 @@ function initializeMap() {
 
 }
 
-/*
-Uncomment the code below when you're ready to implement a Google Map!
-*/
-
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
